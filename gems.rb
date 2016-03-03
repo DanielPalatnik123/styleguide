fb952ed5dbcc8a3103d66894878c634deb9d19bb
Daniel Palatnik <dpalatnik@cipherhealth.com>
2016-03-03 13:55:28 -0500

    added Gemfile

---
+++ gems.rb
@@ -0,0 +1,12 @@
+source "https://rubygems.org"
+
+begin
+  # this trick via:
+  # https://byparker.com/blog/2014/stay-up-to-date-with-the-latest-github-pages-gem/
+  require 'json'
+  require 'open-uri'
+  versions = JSON.parse(open('https://pages.github.com/versions.json').read)
+  gem 'github-pages', versions['github-pages']
+rescue SocketError
+  gem 'github-pages'
+end
